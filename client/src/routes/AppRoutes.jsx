ce08079c776eeda2e654d63823ad971cac642704
sumit kumar <singhsumit0042@gmail.com>
2026-07-27 11:36:58 +0530

    Setup React project with MUI, routing and theme

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Profile from "../pages/Profile/Profile";
+import NotFound from "../pages/NotFound/NotFound";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
