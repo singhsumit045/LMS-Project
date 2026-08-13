66263b8b3353308af0dcbcf40fc79ff2366039c8
sumit kumar <singhsumit0042@gmail.com>
2026-08-13 12:32:47 +0530

    Prepare project for deployment

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const port = process.env.PORT ?? 3000;
+  const port = Number(process.env.PORT) || 3000;
 
   await app.listen(port, '0.0.0.0');
 
